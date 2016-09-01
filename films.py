d4a21504bf1547f57f90b874430700ca82fa6bc5
Eliran Haziza <eliranhaze@gmail.com>
2016-09-01 14:31:39 +0300

    more films and reorganization

--- films.py
+++ films.py
@@ -1,29 +1,43 @@
 
 # films with my ratings
 FILMS = {
+
+     # the best i've seen
     'http://www.metacritic.com/movie/ida': 10,
-    'http://www.metacritic.com/movie/amelie': 9,
     'http://www.metacritic.com/movie/in-the-mood-for-love': 10,
-    'http://www.metacritic.com/movie/chungking-express': 9,
+    'http://www.metacritic.com/movie/a-separation': 10,
     'http://www.metacritic.com/movie/leviathan-2014': 10,
-    'http://www.metacritic.com/movie/winter-sleep': 9.5,
-    'http://www.metacritic.com/movie/once-upon-a-time-in-anatolia': 9.5,
     'http://www.metacritic.com/movie/gett-the-trial-of-viviane-amsalem': 10,
     'http://www.metacritic.com/movie/rams': 10,
+    'http://www.metacritic.com/movie/winter-sleep': 9.5,
+    'http://www.metacritic.com/movie/once-upon-a-time-in-anatolia': 9.5,
     'http://www.metacritic.com/movie/there-will-be-blood': 9.5,
-    'http://www.metacritic.com/movie/son-of-saul': 9,
     'http://www.metacritic.com/movie/carol': 9.5,
+    'http://www.metacritic.com/movie/son-of-saul': 9,
+    'http://www.metacritic.com/movie/spring-summer-fall-winter-and-spring': 9,
+    'http://www.metacritic.com/movie/chungking-express': 9,
+    'http://www.metacritic.com/movie/the-pianist': 9,
+
+    # kubrick
     'http://www.metacritic.com/movie/dr-strangelove-or-how-i-learned-to-stop-worrying-and-love-the-bomb': 9,
     'http://www.metacritic.com/movie/2001-a-space-odyssey': 10,
     'http://www.metacritic.com/movie/a-clockwork-orange': 9,
     'http://www.metacritic.com/movie/eyes-wide-shut': 8,
-    'http://www.metacritic.com/movie/the-salt-of-the-earth': 8,
-    'http://www.metacritic.com/movie/the-tale-of-the-princess-kaguya': 8,
-    'http://www.metacritic.com/movie/a-separation': 10,
-    'http://www.metacritic.com/movie/spring-summer-fall-winter-and-spring': 9,
+
+    # personal favorites
+    'http://www.metacritic.com/movie/amelie': 9,
+    'http://www.metacritic.com/movie/american-beauty': 9,
+
+    # worth watching
+    'http://www.metacritic.com/movie/the-postmans-white-nights': 8.5,
     'http://www.metacritic.com/movie/amour': 8.5,
     'http://www.metacritic.com/movie/the-lives-of-others': 8.5,
     'http://www.metacritic.com/movie/persepolis': 8.5,
+    'http://www.metacritic.com/movie/fargo': 8.5,
+    'http://www.metacritic.com/movie/the-salt-of-the-earth': 8,
+    'http://www.metacritic.com/movie/the-tale-of-the-princess-kaguya': 8,
+    'http://www.metacritic.com/movie/victoria': 8,
+    'http://www.metacritic.com/movie/wild-tales': 8,
     'http://www.metacritic.com/movie/nebraska': 8,
     'http://www.metacritic.com/movie/pans-labyrinth': 8,
     'http://www.metacritic.com/movie/anomalisa': 8,
@@ -31,17 +45,18 @@ FILMS = {
     'http://www.metacritic.com/movie/a-pigeon-sat-on-a-branch-reflecting-on-existence': 8,
     'http://www.metacritic.com/movie/the-lobster': 8,
     'http://www.metacritic.com/movie/the-grand-budapest-hotel': 8,
-    'http://www.metacritic.com/movie/fargo': 8.5,
-    'http://www.metacritic.com/movie/victoria': 8,
-    'http://www.metacritic.com/movie/wild-tales': 8,
-    'http://www.metacritic.com/movie/the-pianist': 9,
-    'http://www.metacritic.com/movie/american-beauty': 9,
+    'http://www.metacritic.com/movie/lincoln': 8,
+    'http://www.metacritic.com/movie/the-return-2004': 8,
 
+    # ok
+    'http://www.metacritic.com/movie/sweet-bean': 7,
     'http://www.metacritic.com/movie/midnight-in-paris': 7,
-    'http://www.metacritic.com/movie/sicario': 5,
-    'http://www.metacritic.com/movie/inside-out-2015': 6.5, 
     'http://www.metacritic.com/movie/captain-fantastic': 7, 
+    'http://www.metacritic.com/movie/inside-out-2015': 6.5, 
     'http://www.metacritic.com/movie/paris-je-taime': 6,
+
+    # not worth it
+    'http://www.metacritic.com/movie/sicario': 5,
     'http://www.metacritic.com/movie/the-academy-of-muses': 4,
     'http://www.metacritic.com/movie/the-nice-guys': 2,
 }
